2a6419c3ed3096774a37cc087e6dc6c0c1c38039
Kesley <kesley@MacBook-Pro-de-TechIn.local>
2020-03-30 15:38:17 -0300

    init build folder

---
+++ build/precache-manifest.2ec87efa0e84dd6f872b96d310244e73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b66f9ced14758e1f98d3c47a6c5f113",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e732f386a55bf0adda56",
+    "url": "/static/css/main.7e03fabd.chunk.css"
+  },
+  {
+    "revision": "f4fc9f821c159df5af0c",
+    "url": "/static/js/2.8c78cec4.chunk.js"
+  },
+  {
+    "revision": "20261ffe8797d840db5edd9bceaa53dd",
+    "url": "/static/js/2.8c78cec4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e732f386a55bf0adda56",
+    "url": "/static/js/main.36c8a478.chunk.js"
+  },
+  {
+    "revision": "7f0677d88f15a519d284",
+    "url": "/static/js/runtime-main.5edffef2.js"
+  },
+  {
+    "revision": "877e35c51bd41c6c3cf772462d7d5eb6",
+    "url": "/static/media/Logo.877e35c5.png"
+  }
+]);
