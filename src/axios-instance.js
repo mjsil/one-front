8a59f82e8151d71cd0ec5b18e736b7c630a8b3dd
Kesley <kesley@MacBook-Pro-de-TechIn.local>
2020-05-04 08:01:05 -0300

    ajustes rotas console

--- src/axios-instance.js
+++ src/axios-instance.js
@@ -3,10 +3,10 @@ import axios from "axios";
 import { getToken } from "./services/auth";
 
 const instance = axios.create({
-  baseURL: "http://167.71.249.90:3331"
+  baseURL: "http://167.71.249.90:3331",
 });
 
-instance.interceptors.request.use(async config => {
+instance.interceptors.request.use(async (config) => {
   const token = getToken();
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
--- src/pages/Console/Layout/Content/routes.js
+++ src/pages/Console/Layout/Content/routes.js
@@ -3,33 +3,39 @@ import MeuPerfil from "../../MeuPerfil/MeuPerfil";
 import UsuariosCadastrados from "../../UsuariosCadastrados/UsuariosCadastrados";
 import MinhaInstituicao from "../../MinhaInstituicao/MinhaInstituicao";
 import MensagemInstitucional from "../../MensagemInstitucional/MensagemInstitucional";
+import Mural from "../../Mural/Mural";
 
 const routes = [
   {
     path: "/console",
     exact: true,
-    component: GridSelection
+    component: GridSelection,
   },
   {
     path: "/console/perfil",
     exact: true,
-    component: MeuPerfil
+    component: MeuPerfil,
   },
   {
     path: "/console/usuarios",
     exact: true,
-    component: UsuariosCadastrados
+    component: UsuariosCadastrados,
   },
   {
     path: "/console/minha-instituicao",
     exact: true,
-    component: MinhaInstituicao
+    component: MinhaInstituicao,
   },
   {
     path: "/console/instituicoes/mensagem",
     exact: true,
-    component: MensagemInstitucional
-  }
+    component: MensagemInstitucional,
+  },
+  {
+    path: "/console/mural/upload",
+    exact: true,
+    component: Mural,
+  },
 ];
 
 export default routes;
