f715f69a377a042f291d6bb0b107adb6426ec749
KesleyK <kesley.kenny.kk@gmail.com>
2020-08-13 16:00:43 -0300

    Autenticacao login

--- src/pages/Auth/Login/Login.js
+++ src/pages/Auth/Login/Login.js
@@ -1,65 +1,72 @@
-import React, { Component, Fragment } from 'react';
-import { setToken } from '../../../services/institution-token';
-import { login } from '../../../services/auth';
-import { withRouter } from 'react-router-dom';
-import axios from '../../../axios-instance';
+import React, { Component, Fragment } from "react";
+import { setToken } from "../../../services/institution-token";
+import { login } from "../../../services/auth";
+import { withRouter } from "react-router-dom";
+import axios from "../../../axios-instance";
 
-import Form from '../../../components/Form/Form/Form';
-import ErrorMessage from '../../../components/Form/ErrorMessage/ErrorMessage';
-import Button from '../../../components/Form/Button/Button';
-import Input from '../../../components/Form/Input/Input';
-import Logo from '../../../components/Form/Logo/Logo';
-import Spinner from '../../../components/UI/Spinner/Spinner';
+import Form from "../../../components/Form/Form/Form";
+import ErrorMessage from "../../../components/Form/ErrorMessage/ErrorMessage";
+import Button from "../../../components/Form/Button/Button";
+import Input from "../../../components/Form/Input/Input";
+import Logo from "../../../components/Form/Logo/Logo";
+import Spinner from "../../../components/UI/Spinner/Spinner";
 
 class Login extends Component {
   state = {
-    email: '',
-    password: '',
+    email: "",
+    password: "",
     errorMessage: null,
-    loading: false
-  }
+    loading: false,
+  };
 
   onChangeInputHandler = (event) => {
     const inputName = event.target.name;
     const inputValue = event.target.value;
     this.setState({
-      [inputName]: inputValue
+      [inputName]: inputValue,
     });
-  }
+  };
 
   onFormSubmitHandler = (event) => {
     event.preventDefault();
     this.setState({ loading: true, errorMessage: null });
     const formData = {
       email: this.state.email,
-      password: this.state.password
+      password: this.state.password,
     };
-    
+
     axios
-      .post('/institutionsessions', formData)
-      .then(res => {
+      .post("/institutionsessions", formData)
+      .then((res) => {
         const jwtToken = res.data.token;
         const user = res.data.user;
-        setToken(user);
-        return login(jwtToken);
-      })
-      .then(() => {
-        this.props.history.replace('/console');
-      })
-      .catch(err => {
-        if (err.response.data) {
-          this.setState({ errorMessage: err.response.data.error})
+
+        if (user.permission !== 2) {
+          return this.setState({
+            loading: false,
+            errorMessage: "Usuário não encontrado.",
+          });
         }
-        this.setState({ loading: false })
+
+        setToken(user);
+        login(jwtToken);
+
+        this.props.history.replace("/console");
+      })
+      .catch((err) => {
+        if (err.response.data) {
+          this.setState({ errorMessage: err.response.data.error });
+        }
+        this.setState({ loading: false });
       });
-  }
+  };
 
   render() {
     let errMessage = null;
     if (this.state.errorMessage) {
-      errMessage = <ErrorMessage>{this.state.errorMessage}</ErrorMessage>
-    } 
-    
+      errMessage = <ErrorMessage>{this.state.errorMessage}</ErrorMessage>;
+    }
+
     let contentToRender = (
       <Fragment>
         {errMessage}
@@ -87,7 +94,7 @@ class Login extends Component {
         </Fragment>
       );
     }
-    
+
     return (
       <Form formOnSubmit={this.onFormSubmitHandler}>
         <Logo />
